f29de1c4e0b4a8e66eedfc5b0b4a05cccac34674
Bevz Tim <tima.bevz@gmail.com>
2020-10-27 22:40:27 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -16,6 +16,25 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    if (shift > 35):
+        shift= shift % 35
+    enF=65
+    enL=90
+    enf=97
+    enl=122
+    for i in range (len(plaintext)):
+        if (ord(plaintext[i]) >= enF and ord(plaintext[i]) <= enL ):
+            char= ord(plaintext[i]) + shift
+            if (char > enL):
+                char=enF+char-enL-1
+            ciphertext+=chr(char)
+        elif (ord(plaintext[i]) >= enf and ord(plaintext[i]) <= enl ):
+            char= ord(plaintext[i]) + shift
+            if (char > enl):
+                char=enf+char-enl-1
+            ciphertext+=chr(char)
+        else:
+            ciphertext+=plaintext[i]
     return ciphertext
 
 
@@ -34,6 +53,25 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    if (shift > 35):
+        shift= shift % 35
+    enF=65
+    enL=90
+    enf=97
+    enl=122
+    for i in range (len(ciphertext)):
+        if (ord(ciphertext[i]) >= enF and ord(ciphertext[i]) <= enL ):
+            char= ord(ciphertext[i]) - shift
+            if (char < enF):
+                char=enL-enF+char+1
+            plaintext+=chr(char)
+        elif (ord(ciphertext[i]) >= enf and ord(ciphertext[i]) <= enl ):
+            char= ord(ciphertext[i]) - shift
+            if (char < enf):
+                char=enl-enf+char+1
+            plaintext+=chr(char)
+        else:
+            plaintext+=ciphertext[i]
     return plaintext
 
 
