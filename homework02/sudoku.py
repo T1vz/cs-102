30ec5d60411ac3c133352d591c8c69aaff41c1a9
Bevz Tim <tima.bevz@gmail.com>
2020-11-24 01:30:56 +0300

    Реализованна функция solve()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -131,12 +131,23 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    pos = find_empty_positions(grid)
+    if pos == None:
+        return grid
+    args = find_possible_values(grid, pos)
+    for i in args:
+        grid[pos[0]][pos[1]] = i
+        if solve(grid):
+            return grid
+        grid[pos[0]][pos[1]] = "."
+    return False
 
 
 def check_solution(solution: List[List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
     # TODO: Add doctests with bad puzzles
+    for i in range(9):
+        
     pass
 
 
